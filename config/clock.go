c489076bdf4b0186eea9ac1c57ebe8d5468371f5
Your Name <you@example.com>
2020-03-16 17:48:22 +0000

    init

--- config/clock.go
+++ config/clock.go
@@ -1,31 +1,114 @@
 package config
 
 
-func ClockFace(clockNum string) []int{
+import (
+	"sort"
+	"github.com/wangbokun/go/log"
+)
+
+
+func ClockFaces(displacement int, clockNum string) []int{
+
+	var t_0 = []int{0, 1, 2, 31, -1, 33, 34, -1, 36, 65, -1, 67, 68, 69, 70}
+	var t_1 = []int{-1, 1, -1, -1, 32, -1, -1, 35, -1, -1, 66, -1, -1, 69, -1}
+	var t_2 = []int{0, 1, 2, -1, -1, 31, 34, 35, 36, 67, -1, -1, 68, 69, 70}
+	var t_3 = []int{0, 1, 2, -1, -1,  31, 34, 35, 36, -1, -1, 65, 68, 69, 70}
+	var t_4 = []int{0, -1, 2, 31, 33, -1, 34, 35, 36, -1, -1, 65, -1, -1, 70}
+	var t_5 = []int{0, 1, 2, 33, -1, -1, 34, 35, 36, -1, -1, 65, 68, 69, 70}
+	var t_6 = []int{0, 1, 2, 33, -1, -1, 34, 35, 36, 65, 67, -1, 68, 69, 70}
+	var t_7 = []int{0, 1, 2, -1, -1, 31, -1, -1, 36, -1, -1, 65, -1, -1, 70}
+	var t_8 = []int{0, 1, 2, 31, 33, -1, 34, 35, 36, 65, 67, -1, 68, 69, 70}
+	var t_9 = []int{0, 1, 2, 31, 33, -1, 34, 35, 36, -1, -1, 65, 68, 69, 70}
 
 	switch clockNum{
-	case "colon":
-		var num = []int{25, 59}
-	case "t_1_0":
-		var num = []int{0, 1, 2, 31, 33, 34, 36, 65, 67, 68, 69, 70}
-	case "t_1_1":
-		var num = []int{1, 32, 35, 66, 69}
-	case "t_1_2":
-		var num = []int{0, 1, 2, 31, 34, 35, 36, 67, 68, 69, 70}
-	case "t_1_3":
-		var num = []int{0, 1, 2, 31, 34, 35, 36, 65, 68, 69, 70}
-	case "t_1_4":
-		var num = []int{0, 2, 31, 33, 34, 35, 36, 65, 70}
-	case "t_1_5":
-		var num = []int{0, 1, 2, 33, 34, 35, 36, 65, 68, 69, 70}
-	case "t_1_6":
-		var num = []int{0, 1, 2, 33, 34, 35, 36, 65, 67, 68, 69, 70}
-	case "t_1_7":
-		var num = []int{0, 1, 2, 31, 36, 65, 70}
-	case "t_1_8":
-		var num = []int{0, 1, 2, 31, 33, 34, 35, 36, 65, 67, 68, 69, 70}
-	case "t_1_9":
-		var num = []int{0, 1, 2, 31, 33, 34, 35, 36, 65, 68, 69, 70}
+	case "t_0":
+		return transform(displacement, t_0)
+	case "t_1":
+		return transform(displacement, t_1)
+	case "t_2":
+		return transform(displacement, t_2)
+	case "t_3":
+		return transform(displacement, t_3)
+	case "t_4":
+		return transform(displacement, t_4)
+	case "t_5":
+		return transform(displacement, t_5)
+	case "t_6":
+		return transform(displacement, t_6)
+	case "t_7":
+		return transform(displacement, t_7)
+	case "t_8":
+		return transform(displacement, t_8)
+	case "t_9":
+		return  transform(displacement, t_9)
+	default:
+		return  nil
 	}
-	return num
+}
+
+
+
+func transform(displacement int, n []int) []int{
+	switch displacement{
+	// 0 
+	case 1:
+		return disNum(0,n)
+	// 4 
+	case 2:
+		return disNum(4,n)
+	// 10 
+	case 3:
+		return disNum(10,n)
+	// 14
+	case 4:
+		return disNum(14,n)
+	}
+	return nil
+}
+
+
+func disNum(d int,n []int) []int{
+
+	log.Error("%d ======= %#v",d,n)
+	if d == 0 {
+		sort.Ints(DeleteSlice(n)[:])
+		return  n
+	}
+
+	var ctx []int
+
+	for k,v := range  n{
+		if v != -1{
+			switch  k{
+			case  3:
+				ctx = append(ctx,v - d)
+			case  4:
+				ctx = append(ctx,v - d)
+			case  5:
+				ctx = append(ctx,v - d)
+			case  9:
+				ctx = append(ctx,v - d)
+			case  10:
+				ctx = append(ctx,v - d)
+			case  11:
+				ctx = append(ctx,v - d)
+			default:
+				ctx = append(ctx,v + d)
+			}
+		}
+	}
+
+	sort.Ints(DeleteSlice(ctx)[:])
+	return  ctx
+}
+
+
+func DeleteSlice(a []int) []int{
+	for i := 0; i < len(a); i++ {
+		if a[i] == -1 {
+			a = append(a[:i], a[i+1:]...)
+			i--
+		}
+	}
+	return a
 }
--- main.go
+++ main.go
@@ -15,17 +15,16 @@ const (
 	r =  uint32(0x002000)
 	g =  uint32(0x200000)
 	b =  uint32(0x000020)
+
+	b1 =  uint32(0x100800)
+	b2 =  uint32(0x100008)
+	b3 =  uint32(0x081000)
+	b4 =  uint32(0x080010)
+	b5 =  uint32(0x001008)
+	b6 =  uint32(0x000810)
 )
 
-
-//var h0 = []int{1, 2, 3, 18, 20, 35, 37, 52, 54, 69, 70, 71}
-var t_1_0 = []int{0, 1, 2, 33, 31, 34, 36, 67, 65, 68, 69, 70}
-var t_2_0 = []int{4, 5, 6, 29, 27, 38, 40, 63, 61, 72, 73, 74}
-
-var d = []int{25, 59}
-
-var t_3_2 = []int{10, 11, 12, 21, 44, 45, 46, 57, 78, 79, 80}
-var t_4_2 = []int{14, 15, 16, 17, 48, 49, 50, 53, 82, 83, 84}
+var t = []string{"t_0","t_1", "t_2", "t_3", "t_4", "t_5", "t_6", "t_7", "t_8","t_9"}
 
 
 func colorWipe(num int,color uint32) error{
@@ -40,43 +39,40 @@ func colorWipe(num int,color uint32) error{
 }
 
 
-func led(n []int,color uint32) {
+func led(t time.Duration, n []int, color uint32) {
 	for _,v := range n {
-		fmt.Print("----",v)
 		err  := colorWipe(v,color)
 		if err != nil{
 			fmt.Println(err)
 		}
-	        time.Sleep(30 * time.Millisecond)
+	        time.Sleep(t)
 	}
 }
 
 func main(){
 
-     defer ws2811.Fini()
      err := ws2811.Init(pin, count, brightness)
 
      if err != nil{
-	fmt.Println(err)	
+	fmt.Println(err)
      }	else {
+	for  _,v1 := range t {
+		n1 := config.ClockFaces(1,v1)
+		if n1 !=  nil{
+			led(30 * time.Millisecond,n1,b1)
+			ws2811.Clear()
+		}
 
-        t_1_0 = append(t_1_0, t_2_0...)
-        t_1_0 = append(t_1_0, d...)
-        t_1_0 = append(t_1_0, t_3_2...)
-        t_1_0 = append(t_1_0, t_4_2...)
-
-	led(t_1_0,r)
-	time.Sleep(50 * time.Millisecond)
-	led(t_1_0,g)
-	time.Sleep(50 * time.Millisecond)
-	led(t_1_0,b)
-
-	var ti = []string{"t_1_0","t_1_1", "t_1_2", "t_1_3", "t_1_4", "t_1_5", "t_1_6", "t_1_7", "t_1_8","t_1_9"}
-	for  _,v := range ti {
-		t := config.ClockFace(v)
-		led(t,r)
-		time.Sleep(50 * time.Millisecond)
+		for _,v2 := range t{
+			n2 := config.ClockFaces(2,v2)
+			fmt.Println("n1======:",n1)
+			fmt.Println("n2>>>>>>>:",n2)
+			if n2 != nil{
+				led(5 * time.Millisecond,n1,b2)
+				led(30 * time.Millisecond,n2,b3)
+				ws2811.Clear()
+			}
+		}
 	}
-
-     }
+    }
 }
